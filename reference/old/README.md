78980a2adba01da03acdd4ac38445c2c3940d8b8
josh <JoshuaJDHedges@gmail.com>
2018-09-15 15:30:16 -0600

    Removed reference directory and contents

--- reference/old/README.md
+++
@@ -1,2 +0,0 @@
-# spaceman
-A rogue-like game based on Ludum Dare 39: "Running out of Power".
--- reference/old/app/core/set.lua
+++
@@ -1,9 +0,0 @@
-
-
-function cleanNils(t)
-  local ans = {}
-  for _,v in pairs(t) do
-    ans[ #ans+1 ] = v
-  end
-  return ans
-end
